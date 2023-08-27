1ba0e26ce30e292564816924c541a15a1b5b1387
anjeljayswal <53367474+anjeljayswal@users.noreply.github.com>
2023-08-27 19:27:55 +0545

    Update FormB.js

--- src/components/FormB.js
+++ src/components/FormB.js
@@ -2,34 +2,44 @@ import { useState, useEffect } from "react";
 function FormB({ onSubmit, age }) {
     const [marvelShows, setMarvelShows] = useState("");
 
-    const handleSubmit = (event) => {
+    const handleSubmit =  (e) => {
+    e.preventDefault();
+    const formData = {
+      formType: "Form B",
+      age,
+      marvelShows,
     };
+    onSubmit(formData);
+  };
 
 
 
     return (
-        <form id="marvel" >
-            <h2>Form B</h2>
-            <label>
-                Select Marvel Shows:
-                <select  >
-                    <option value="">--Select--</option>
-                    <option value="WandaVision">WandaVision</option>
-                    <option value="The Falcon and the Winter Soldier">
-                        The Falcon and the Winter Soldier
-                    </option>
-                    <option value="Loki">Loki</option>
-                    <option value="What If...?">What If...?</option>
-                </select>
-            </label>
-            <br />
-            <label>
-                Enter your age:
-                <input type="number" value={age} disabled />
-            </label>
-            <br />
-            <button id="submit-marvel" type="submit">Submit</button>
-        </form>
+       <form id="marvel" onSubmit={handleSubmit}>
+      <h2>Form B</h2>
+      <label>
+        Select Marvel Shows:
+        <select onChange={(e) => setMarvelShows(e.target.value)}>
+          <option value="">--Select--</option>
+          <option value="WandaVision">WandaVision</option>
+          <option value="The Falcon and the Winter Soldier">
+            The Falcon and the Winter Soldier
+          </option>
+          <option value="Loki">Loki</option>
+          <option value="What If...?">What If...?</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Enter your age:
+        <input type="number" value={age} disabled />
+      </label>
+      <br />
+      <button id="submit-marvel" type="submit">
+        Submit
+      </button>
+    </form>
+
     );
 }
 export default FormB;
