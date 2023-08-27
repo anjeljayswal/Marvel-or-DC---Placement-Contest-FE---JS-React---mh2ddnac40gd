1c429c61f67b83f7379c7dd3bc21e1a489a0d30e
anjeljayswal <53367474+anjeljayswal@users.noreply.github.com>
2023-08-27 19:29:03 +0545

    Update FormA.js

--- src/components/FormA.js
+++ src/components/FormA.js
@@ -1,30 +1,38 @@
 import { useState } from "react";
 
 function FormA({ onSubmit, age }) {
-    const handleSubmit = (event) => {
+    const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      formType: "Form A",
+      age,
+      dcShow,
+        };
+    onSubmit(formData);
     };
     return (
         <form id="dc" onSubmit={handleSubmit}>
-            <h2>Form A</h2>
-            <label>
-                Select DC Shows:
-                <select  >
-                    <option value="">--Select--</option>
-                    <option value="The Flash">The Flash</option>
-                    <option value="Arrow">Arrow</option>
-                    <option value="Supergirl">Supergirl</option>
-                    <option value="Legends of Tomorrow">Legends of Tomorrow</option>
-                </select>
-            </label>
-            <br />
-            <label>
-                Enter your age:
-                <input type="number" value={age} disabled />
-            </label>
-            <br />
-            <button id="submit-dc" type="submit">Submit</button>
-
-        </form>
+      <h2>Form A</h2>
+      <label>
+        Select DC Shows:
+        <select onChange={(e) => setDcShow(e.target.value)}>
+          <option value="">--Select--</option>
+          <option value="The Flash">The Flash</option>
+          <option value="Arrow">Arrow</option>
+          <option value="Supergirl">Supergirl</option>
+          <option value="Legends of Tomorrow">Legends of Tomorrow</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Enter your age:
+        <input type="number" value={age} disabled />
+      </label>
+      <br />
+      <button id="submit-dc" type="submit">
+        Submit
+      </button>
+    </form>
     );
 }
 export default FormA;
